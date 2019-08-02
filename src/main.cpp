c62e52509839c0c1eb51f1691464e81f61f430bb
Luis Uranga <luisuranga.urangaruiz@altran.com>
2019-08-02 19:42:07 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 THINGER LTD
+// Author: alvarolb@gmail.com (Alvaro Luis Bustamante)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "thinger/thinger.h"
+#include <time.h>
+#include <stdlib.h>
+
+#define USER_ID             "lmokto"
+#define DEVICE_ID           "asdasd"
+#define DEVICE_CREDENTIAL   "slg33N0esC6H"
+
+int main(int argc, char *argv[])
+{
+
+    thinger_device thing(USER_ID, DEVICE_ID, DEVICE_CREDENTIAL);
+
+
+    thing["sum"] = [](pson& in, pson& out){
+        out["result"] = rand() % 20;
+    };
+
+    thing["sum2"] = [](pson& in, pson& out){
+        out["result"] = rand() % 20;
+    };
+
+    thing.start();
+    return 0;
+}
