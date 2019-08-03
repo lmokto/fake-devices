8639a616fede9fa3f5f5357d2c330c82d9b2b372
Luis Uranga <luisuranga.urangaruiz@altran.com>
2019-08-03 02:53:39 +0200

    change folder to clone devices

--- devices/dev1/src/main.cpp
+++ devices/dev1/src/main.cpp
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include "thinger/thinger.h"
+#include "../../../src/thinger/thinger.h"
 #include <time.h>
 #include <stdlib.h>
 
--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DDAEMON=OFF ../
-make thinger
-./thinger
+ 
